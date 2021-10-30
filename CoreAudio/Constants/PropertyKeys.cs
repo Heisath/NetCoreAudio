3ccfe7c60602a658dcea4597006db26468bd3c60
Heisath <jannis@imserv.org>
2021-10-30 18:19:41 +0200

    Update PropertyKeys

--- CoreAudio/Constants/PropertyKeys.cs
+++ CoreAudio/Constants/PropertyKeys.cs
@@ -17,6 +17,7 @@
 // THE SOFTWARE.
 // -----------------------------------------
 
+using CoreAudio.Externals;
 using System;
 
 namespace CoreAudio.Constants
@@ -24,8 +25,8 @@ namespace CoreAudio.Constants
 	/// <summary>
 	/// Defines all applicable property keys for CoreAudio API.
 	/// </summary>
-    public static class PropertyKeys
-    {
+	public static class PropertyKeys
+	{
 		/* Defined by MMDevice API */
 		/* ----------------------- */
 
@@ -39,30 +40,30 @@ namespace CoreAudio.Constants
 		/// </summary>
 		public static readonly Guid PKEY_AudioEndpoint_ControlPanelPageProvider = new Guid(0x1da5d803, 0xd492, 0x4edd, 0x8c, 0x23, 0xe0, 0xc0, 0xff, 0xee, 0x7f, 0x0e);
 
-        /// <summary>
+		/// <summary>
 		/// Associates a kernel-streaming (KS) pin category with an audio endpoint device.
-        /// </summary>
-        public static readonly Guid PKEY_AudioEndpoint_Association = new Guid(0x1da5d803, 0xd492, 0x4edd, 0x8c, 0x23, 0xe0, 0xc0, 0xff, 0xee, 0x7f, 0x0e);
+		/// </summary>
+		public static readonly Guid PKEY_AudioEndpoint_Association = new Guid(0x1da5d803, 0xd492, 0x4edd, 0x8c, 0x23, 0xe0, 0xc0, 0xff, 0xee, 0x7f, 0x0e);
 
-        /// <summary>
+		/// <summary>
 		/// Defines the physical speaker configuration for the audio endpoint device.
-        /// </summary>
-        public static readonly Guid PKEY_AudioEndpoint_PhysicalSpeakers = new Guid(0x1da5d803, 0xd492, 0x4edd, 0x8c, 0x23, 0xe0, 0xc0, 0xff, 0xee, 0x7f, 0x0e);
+		/// </summary>
+		public static readonly Guid PKEY_AudioEndpoint_PhysicalSpeakers = new Guid(0x1da5d803, 0xd492, 0x4edd, 0x8c, 0x23, 0xe0, 0xc0, 0xff, 0xee, 0x7f, 0x0e);
 
-        /// <summary>
+		/// <summary>
 		/// Supplies the DirectSound device identifier that corresponds to the audio endpoint device.
-        /// </summary>
-        public static readonly Guid PKEY_AudioEndpoint_GUID = new Guid(0x1da5d803, 0xd492, 0x4edd, 0x8c, 0x23, 0xe0, 0xc0, 0xff, 0xee, 0x7f, 0x0e);
+		/// </summary>
+		public static readonly Guid PKEY_AudioEndpoint_GUID = new Guid(0x1da5d803, 0xd492, 0x4edd, 0x8c, 0x23, 0xe0, 0xc0, 0xff, 0xee, 0x7f, 0x0e);
 
-        /// <summary>
+		/// <summary>
 		/// Specifies whether system effects are enabled in the shared-mode stream that flows to or from the audio endpoint device.
-        /// </summary>
-        public static readonly Guid PKEY_AudioEndpoint_Disable_SysFx = new Guid(0x1da5d803, 0xd492, 0x4edd, 0x8c, 0x23, 0xe0, 0xc0, 0xff, 0xee, 0x7f, 0x0e);
+		/// </summary>
+		public static readonly Guid PKEY_AudioEndpoint_Disable_SysFx = new Guid(0x1da5d803, 0xd492, 0x4edd, 0x8c, 0x23, 0xe0, 0xc0, 0xff, 0xee, 0x7f, 0x0e);
 
-        /// <summary>
+		/// <summary>
 		/// Specifies the channel-configuration mask for the full-range speakers that are connected to the audio endpoint device.
-        /// </summary>
-        public static readonly Guid PKEY_AudioEndpoint_FullRangeSpeakers = new Guid(0x1da5d803, 0xd492, 0x4edd, 0x8c, 0x23, 0xe0, 0xc0, 0xff, 0xee, 0x7f, 0x0e);
+		/// </summary>
+		public static readonly Guid PKEY_AudioEndpoint_FullRangeSpeakers = new Guid(0x1da5d803, 0xd492, 0x4edd, 0x8c, 0x23, 0xe0, 0xc0, 0xff, 0xee, 0x7f, 0x0e);
 
 		/// <summary>
 		/// Indicates whether the endpoint supports the event-driven mode.
@@ -73,11 +74,11 @@ namespace CoreAudio.Constants
 		/// Contains an output category GUID for an audio endpoint device. 
 		/// </summary>
 		public static readonly Guid PKEY_AudioEndpoint_JackSubType = new Guid(0x1da5d803, 0xd492, 0x4edd, 0x8c, 0x23, 0xe0, 0xc0, 0xff, 0xee, 0x7f, 0x0e);
-        
+
 		/// <summary>
 		/// Specifies the device format, which is the format that the audio engine uses for the shared-mode stream that flows to or from the audio endpoint device.
-        /// </summary>
-        public static readonly Guid PKEY_AudioEngine_DeviceFormat = new Guid(0xf19f064d, 0x82c, 0x4e27, 0xbc, 0x73, 0x68, 0x82, 0xa1, 0xbb, 0x8e, 0x4c);
+		/// </summary>
+		public static readonly Guid PKEY_AudioEngine_DeviceFormat = new Guid(0xf19f064d, 0x82c, 0x4e27, 0xbc, 0x73, 0x68, 0x82, 0xa1, 0xbb, 0x8e, 0x4c);
 
 		/// <summary>
 		/// Specifies the default format of the device that is used for rendering or capturing a stream.
@@ -90,14 +91,27 @@ namespace CoreAudio.Constants
 		/// <summary>
 		/// Contains the friendly name of the endpoint device.
 		/// </summary>
-		public static readonly Guid PKEY_Device_FriendlyName = new Guid(0xa45c254e, 0xdf1c, 0x4efd, 0x80, 0x20, 0x67, 0xd1, 0x46, 0xa8, 0x50, 0xe0); // PID 14
+		public static readonly PROPERTYKEY PKEY_Device_FriendlyName = new PROPERTYKEY()
+		{
+			fmtid = new Guid(0xa45c254e, 0xdf1c, 0x4efd, 0x80, 0x20, 0x67, 0xd1, 0x46, 0xa8, 0x50, 0xe0),
+			pid = 14
+		};
+
 		/// <summary>
 		/// Contains the friendly name of the endpoint interface.
 		/// </summary>
-		public static readonly Guid PKEY_DeviceInterface_FriendlyName = new Guid(0x026e516e, 0xb814, 0x414b, 0x83, 0xcd, 0x85, 0x6d, 0x6f, 0xef, 0x48, 0x22); // PID 2
+		public static readonly PROPERTYKEY PKEY_DeviceInterface_FriendlyName = new PROPERTYKEY() {
+			fmtid = new Guid(0x026e516e, 0xb814, 0x414b, 0x83, 0xcd, 0x85, 0x6d, 0x6f, 0xef, 0x48, 0x22),
+			pid = 2
+		};
+
 		/// <summary>
 		/// Contains the description of the endpoint device.
 		/// </summary>
-		public static readonly Guid PKEY_Device_DeviceDesc = new Guid(0xa45c254e, 0xdf1c, 0x4efd, 0x80, 0x20, 0x67, 0xd1, 0x46, 0xa8, 0x50, 0xe0); // PID 2
+		public static readonly PROPERTYKEY PKEY_Device_DeviceDesc = new PROPERTYKEY()
+		{
+			fmtid = new Guid(0xa45c254e, 0xdf1c, 0x4efd, 0x80, 0x20, 0x67, 0xd1, 0x46, 0xa8, 0x50, 0xe0),
+			pid = 2
+		};
 	}
 }
